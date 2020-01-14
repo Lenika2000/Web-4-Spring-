f14a0ca7a57748e31a8b416d10563c5d5ec515ba
DeltaThreeEight <lenika2000@yandex.ru>
2020-01-14 12:05:44 +0300

    Обработка некорректных url паттернов

--- src/main/java/app/controller/StaticResourcesController.java
+++ src/main/java/app/controller/StaticResourcesController.java
@@ -1,12 +1,14 @@
 package app.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
-public class StaticResourcesController {
+public class StaticResourcesController implements ErrorController {
     @GetMapping("/auth/*")
     public String auth(Map<String, Object> model) {
         return "/index.html";
@@ -15,5 +17,15 @@ public class StaticResourcesController {
     @GetMapping("/main")
     public String main(Map<String, Object> model) {
         return "/index.html";
+    }
+
+    @GetMapping("/error")
+    public String errors(Map<String, Object> model) {
+        return "/index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
